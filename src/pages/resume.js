5d37384a8ae4db39c10f32a5d155beedb427fdcc
CJ Dunteman <cjdunteman@gmail.com>
2021-09-04 12:29:26 -0700

    Create resume.js

---
+++ src/pages/resume.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Resume() {
+  return (
+    <>
+      <iframe frameborder="0" scrolling="no" src="../../../static/cjdunteman_resume.pdf"</iframe>
+    <>
+      
