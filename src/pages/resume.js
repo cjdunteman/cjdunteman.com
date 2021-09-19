870056e94d02d79931b4cb1d5fe9d091a095db30
CJ Dunteman <cjdunteman@gmail.com>
2021-09-19 15:53:17 -0700

    Delete resume.js

--- src/pages/resume.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-export default function Resume() {
-  return (
-    <>
-      <iframe frameborder="0" scrolling="no" src="../../../static/cjdunteman_resume.pdf"</iframe>
-    <>
-      
