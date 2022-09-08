77041a74108062eee936d1ac964019a3a73f07f3
CJ Dunteman <cjdunteman@gmail.com>
2022-09-08 11:15:38 -0500

    Add Head element

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,18 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "next-themes";
 import Inspect from "inspx";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>CJ Dunteman</title>
+      <meta charSet="UTF-8"/>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="description" content="CJ Dunteman's personal developer website."/>
+    </Head>
     <Inspect margin size padding>
       <ThemeProvider attribute="class">
         <Layout>
@@ -12,6 +21,7 @@ function MyApp({ Component, pageProps }) {
         </Layout>
       </ThemeProvider>
     </Inspect>
+    </>
   );
 }
 
