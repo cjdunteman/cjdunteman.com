ff634d7cbb8ff37fe131ed71ec9570887776bb64
CJ Dunteman <cjdunteman@gmail.com>
2022-12-03 15:06:02 -0600

    OG image for home

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,14 @@ export default async function Head() {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="CJ Dunteman's personal website." />
+
+        <meta property='og:title' content="CJ Dunteman" />
+        <meta property='og:description' content="CJ's personal website" />
+        <meta property='og:url' content={`https://cjdunteman.com`} />
+        <meta
+            property="og:image"
+            content={`https://cjdunteman.com/api/og?title=${post.title}`}
+        />
       </>
     )
   }
