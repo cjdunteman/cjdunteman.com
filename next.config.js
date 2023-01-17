6bf627915155061e162da9cd3bd8de8422bc75b9
CJ Dunteman <cjdunteman@gmail.com>
2023-01-16 20:11:25 -0600

    Add CSP and robots.txt

--- next.config.js
+++ next.config.js
@@ -17,6 +17,33 @@ const nextConfig = {
   }
 };
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  `
+
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'microphone=(), geolocation=(), camera=()',
+  }
+]
+
 module.exports = withContentlayer({
   async redirects() {
     return [
@@ -41,6 +68,15 @@ module.exports = withContentlayer({
         permanent: true,
       },
     ];
+  },
+  async headers () {
+    return [
+      {
+        // Apply these to all routes in your application
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
   ...nextConfig,
 });
