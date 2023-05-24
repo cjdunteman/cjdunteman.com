329e259f2ee89afeedf3b31484238b8ac51b3637
CJ Dunteman <cjdunteman@gmail.com>
2023-05-24 11:47:49 -0500

    Working?

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,8 +2,8 @@ import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
 import { authOptions } from '../api/auth/[...nextauth]/route'
 
-export default async function Account() {
-    const session = await getServerSession(authOptions)
+export default async function Account(req, res) {
+    const session = await getServerSession(req, res, authOptions)
 
     if (!session) {
         redirect('/signin?callbackUrl=/account')
