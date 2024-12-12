61f2357b345891c31225b92d0f3f4e52a2e70622
CJ Dunteman <cjdunteman@gmail.com>
2024-12-11 20:46:33 -0600

    Reset

--- app/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-// TODO: project page
-export default function Projects() {
-    return (
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
-}
--- cypress/components/CommentForm.cy.js
+++
@@ -1,7 +0,0 @@
-import CommentForm from "components/CommentForm";
-
-it("should take input", () => {
-  cy.mount(<CommentForm />);
-  cy.get('input[name="comment"]').type("Example comment.");
-  //   cy.get("button").should("contains.text", "Submit");
-});
