309d59393cf8c04df23adf74f7653e88af14e997
CJ Dunteman <cjdunteman@gmail.com>
2025-08-09 15:14:26 -0500

    Update contact form

---
+++ src/pages/actions/send-email.ts
@@ -0,0 +1,31 @@
+import { ActionError, defineAction } from 'astro:actions';
+import { z } from 'astro:schema';
+import { Resend } from 'resend';
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const server = {
+  send: defineAction({
+    accept: 'form',
+    input: z.object({
+      msg: z.string(),
+    }),
+    handler: async ( msg ) => {
+      const { data, error } = await resend.emails.send({
+        from: 'Contact Form <contact@cjdunteman.com>',
+        to: ['cjdunteman@gmail.com'],
+        subject: 'New Contact Form Submission',
+        html: `<strong>Message:</strong><br>${msg}`,
+      })
+
+      if (error) {
+        throw new ActionError({
+          code: 'BAD_REQUEST',
+          message: error.message,
+        });
+      }
+
+      return data;
+    },
+  }),
+};
