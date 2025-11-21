18bb69c6138fc62e8ddfb755a888a5bb117b62af
CJ Dunteman <cjdunteman@gmail.com>
2025-11-20 23:59:03 -0600

    Switch to node adapter from vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
 
 import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 export default defineConfig({
   experimental: {
     chromeDevtoolsWorkspace: true,
   },
+
   output: "static",
-  adapter: vercel(),
   site: "https://cjdunteman.com",
   integrations: [sitemap()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
